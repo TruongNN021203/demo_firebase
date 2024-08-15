53928ff169190ef3ac5c195f73d12d9c4fa3d08d
Nguyen Nhat Truong <truongnnce171514@fpt.edu.vn>
2024-08-15 15:26:55 +0700

    demo

---
+++ My_Demo/src/configs/firebase.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import "firebase/storage";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCthgbfZRMLeuCZTnCBQOgblzn9GhJnIqI",
+  authDomain: "mydemo-c64be.firebaseapp.com",
+  projectId: "mydemo-c64be",
+  storageBucket: "mydemo-c64be.appspot.com",
+  messagingSenderId: "542028692961",
+  appId: "1:542028692961:web:e371751106144cbc2966a0",
+  measurementId: "G-G4TYVD7WXK",
+};
+firebase.initializeApp(firebaseConfig); //khởi tạo firebase app function
+
+const projectAuth = firebase.auth();
+const projectFirestore = firebase.firestore();
+const projectStorage = firebase.storage();
+
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectAuth, projectFirestore, projectStorage, timestamp };
